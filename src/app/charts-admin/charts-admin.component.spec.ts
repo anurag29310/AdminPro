70a6bfbd2adf0a13f4998e16e0c51ba081e12cc2
anurag <anurag.singh@ganitsoft.com>
2020-04-13 20:44:10 +0530

    New Add

---
+++ src/app/charts-admin/charts-admin.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChartsAdminComponent } from './charts-admin.component';
+
+describe('ChartsAdminComponent', () => {
+  let component: ChartsAdminComponent;
+  let fixture: ComponentFixture<ChartsAdminComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChartsAdminComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChartsAdminComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/charts-admin/charts-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-charts-admin',
+  templateUrl: './charts-admin.component.html',
+  styleUrls: ['./charts-admin.component.css']
+})
+export class ChartsAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
